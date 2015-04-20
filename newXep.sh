492baf1a4b0a2b587a3fbba3d6a78298a241d607
Florian Schmaus <flo@geekplace.eu>
2015-04-20 17:12:30 +0200

    Remove xep-template.xml in newXep.sh
    otherwise we end up with to .xml XEPs in the new XEPs directory.

--- newXep.sh
+++ newXep.sh
@@ -6,6 +6,7 @@ set -x
 XEP_SHORTNAME=$1
 
 cp -r xep-template xep-${XEP_SHORTNAME}
+rm xep-${XEP_SHORTNAME}/xep-template.xml
 cp xmpp/extensions/xep-template.xml xep-${XEP_SHORTNAME}/xep-${XEP_SHORTNAME}.xml
 sed -i "s/template/${XEP_SHORTNAME}/g" xep-${XEP_SHORTNAME}/Makefile
 git add xep-${XEP_SHORTNAME}
