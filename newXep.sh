777e34d27b7b5882454aacde436b484db61ec160
Florian Schmaus <flo@geekplace.eu>
2015-11-17 10:19:15 +0100

    Add OpenPGP XEP

--- newXep.sh
+++ newXep.sh
@@ -7,6 +7,6 @@ XEP_SHORTNAME=$1
 
 cp -r xep-template "xep-${XEP_SHORTNAME}"
 rm "xep-${XEP_SHORTNAME}/xep-template.xml"
-cp xmpp/extensions/xep-template.xml "xep-${XEP_SHORTNAME}/xep-${XEP_SHORTNAME}.xml"
+cp xsf-xeps/xep-template.xml "xep-${XEP_SHORTNAME}/xep-${XEP_SHORTNAME}.xml"
 sed -i "s/template/${XEP_SHORTNAME}/g" "xep-${XEP_SHORTNAME}/Makefile"
 git add "xep-${XEP_SHORTNAME}"
